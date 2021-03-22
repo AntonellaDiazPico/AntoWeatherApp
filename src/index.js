d05d2692d2468501acec3e18980bc79df2956326
AntonellaDiazPico <antonelladiazpico@gmail.com>
2021-03-23 09:13:26 +1100

    Added JS to to forecast with Geolocation

--- src/index.js
+++ src/index.js
@@ -147,6 +147,10 @@ function getLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
 
   axios.get(apiUrl).then(displayCurrentWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 // FORECAST
